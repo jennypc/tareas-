35854abf293fe63692055cdaf49ddb7400e499e8
Pamela <pamela1708.jpc@gmail.com>
2018-02-05 15:37:22 -0600

    Borre comentarios por que no se utilizaban

--- Calculadora/main.c
+++ Calculadora/main.c
@@ -1,15 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: UNIVERIDAD
- *
- * Created on 17 de septiembre de 2017, 10:16 AM
- */
 
 #include <stdio.h>
 #include <stdlib.h>
